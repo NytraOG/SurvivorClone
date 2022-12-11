28a269160ea8f78c9709381888ae21ac72c5cedf
Christian Nytra <christian.nytra@co-orga.d>
2022-12-11 01:40:00 +0100

    basic playermovement fertig

--- Co-Orga Survivors/Assets/Scripts/Player.cs
+++ Co-Orga Survivors/Assets/Scripts/Player.cs
@@ -1,22 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using ScriptableObjects;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public BaseSurvivor survivor;
+    public         BaseSurvivor survivor;
+    public         float        moveSpeed;
+    private static float        HorizontalInput => Input.GetAxis("Horizontal");
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         var spriteRenderer = GetComponent<SpriteRenderer>();
         spriteRenderer.sprite = survivor.sprite;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        var rigidBodyComponent = GetComponent<Rigidbody2D>();
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            rigidBodyComponent.transform.position = transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            rigidBodyComponent.transform.position = transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        else if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            rigidBodyComponent.transform.position = transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            rigidBodyComponent.transform.position = transform.position += Vector3.down * moveSpeed * Time.deltaTime;
     }
+
+    private void OnCollisionEnter2D(Collision2D col) => Debug.Log($"Collision with {col.gameObject.name}");
 }
