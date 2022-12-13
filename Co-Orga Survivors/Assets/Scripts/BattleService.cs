c689ac7d789198ff61b5275e4dd835936d6adc0e
Christian Nytra <christian.nytra@co-orga.d>
2022-12-13 23:22:27 +0100

    Gegnergruppenspawn rudimentär fertig

--- Co-Orga Survivors/Assets/Scripts/BattleService.cs
+++ Co-Orga Survivors/Assets/Scripts/BattleService.cs
@@ -1,17 +1,75 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class BattleService : MonoBehaviour
 {
-    public GameObject playerPrefab;
-    public GameObject enemyNullreferenceExceptionPrefab;
+    public  GameObject enemyNullreferenceExceptionPrefab;
+    public  float      timeToSpawnFirstWave;
+    public  float      timeToSpawnConsecutiveWaves;
+    public  int        amountOfEnemiesToSpawn;
+    public  int        spawnSafetyRadius;
+    public  float      spawnDistanceBetweenEnemiesOfGroup;
+    private bool       firstWaveWasSpawned;
+    private float      middleX;
+    private float      middleY;
+    private Player     player;
+    private float      ticks;
+    private float      Elapsed => ticks % 60;
 
-    // Start is called before the first frame update
     private void Start()
     {
-        //Instantiate(playerPrefab);
-        //Instantiate(enemyNullreferenceExceptionPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        player = GameObject.Find(nameof(Player))?.GetComponent<Player>();
+        var playerposition = player.transform.position;
+
+        middleX = Math.Abs(playerposition.x);
+        middleY = Math.Abs(playerposition.y);
     }
 
-    // Update is called once per frame
-    private void Update() { }
+    private void Update()
+    {
+        if (!firstWaveWasSpawned && Elapsed >= timeToSpawnFirstWave)
+        {
+            SpawnEnemies();
+        }
+        else if (Elapsed >= timeToSpawnConsecutiveWaves)
+        {
+            SpawnEnemies();
+            ticks = 0;
+        }
+    }
+
+    private void SpawnEnemies()
+    {
+        var distanceModifier = Random.Range(1f, 3f);
+        var firstSpawnpoint  = new Vector3(middleX * distanceModifier * GetVorzeichen(), middleY * distanceModifier * GetVorzeichen(), -1);
+
+        Instantiate(enemyNullreferenceExceptionPrefab, firstSpawnpoint, Quaternion.identity);
+
+        for (var i = 0; i < amountOfEnemiesToSpawn - 1; i++)
+        {
+            //Einmal bestimmen, ob der gegner links, recht oder gemischt vom firstSpawn spawnt
+            var enemyPosition = GetEnemyPosition(Random.Range(1, 3), Random.Range(0.5f, 1.01f), firstSpawnpoint, GetVorzeichen());
+            Instantiate(enemyNullreferenceExceptionPrefab, enemyPosition, Quaternion.identity);
+        }
+
+        firstWaveWasSpawned = true;
+        ticks               = 0;
+    }
+
+    private void FixedUpdate() => ticks += Time.deltaTime;
+
+    private static int GetVorzeichen()
+    {
+        var vorzeichen = Random.Range(0, 2) == 0 ? 1 : -1;
+        return vorzeichen;
+    }
+
+    private Vector3 GetEnemyPosition(int spawnIndex, float distanceModifier, Vector3 originalPosition, int vorzeichen) => spawnIndex switch
+    {
+        1 => originalPosition + new Vector3(spawnDistanceBetweenEnemiesOfGroup * distanceModifier * vorzeichen, 0),
+        2 => originalPosition + new Vector3(0, spawnDistanceBetweenEnemiesOfGroup * distanceModifier * vorzeichen),
+        3 => originalPosition + new Vector3(spawnDistanceBetweenEnemiesOfGroup * 0.7f * distanceModifier * vorzeichen, spawnDistanceBetweenEnemiesOfGroup * 0.7f * distanceModifier * vorzeichen),
+        _ => throw new ArgumentException(nameof(spawnIndex))
+    };
 }
--- Co-Orga Survivors/Assets/Scripts/EnemyNullreferenceException.cs
+++ Co-Orga Survivors/Assets/Scripts/EnemyNullreferenceException.cs
@@ -7,10 +7,29 @@ public class EnemyNullreferenceException : BaseUnit
     // Start is called before the first frame update
     private void Start()
     {
-        player            = GameObject.Find(nameof(Player))?.GetComponent<Player>();
-        expGrantedOnDeath = 2;
+        player = GameObject.Find(nameof(Player))?.GetComponent<Player>();
     }
 
     // Update is called once per frame
-    private void Update() { }
+    private void Update()
+    {
+        var body = GetComponent<Rigidbody2D>();
+        var targetPosition  = player.transform.position;
+        var thisPosition    = transform.position;
+        var directionToMove = targetPosition - thisPosition;
+
+        directionToMove = directionToMove.normalized * Time.deltaTime * moveSpeed;
+
+        body.transform.position = thisPosition += directionToMove * moveSpeed * Time.deltaTime;
+        //transform.position      = thisPosition;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // if (collision.gameObject.name.Contains("Sexy"))
+        //     return;
+        // // var enemy =  collision.gameObject. as BaseUnit;
+        // var a = GetComponent<AudioSource>();
+        // a.Play();
+    }
 }
