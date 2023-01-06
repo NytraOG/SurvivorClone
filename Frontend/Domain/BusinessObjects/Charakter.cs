f5b8b23bedd81c224625aa538054ef0d94d47de9
Tobias <tlemster@outlook.com>
2023-01-07 00:37:08 +0100

    Mitarbeiterauswahl improved =)

--- Frontend/Domain/BusinessObjects/Charakter.cs
+++ Frontend/Domain/BusinessObjects/Charakter.cs
@@ -5,7 +5,11 @@ namespace CoOrga.Survivors.Domain.BusinessObjects;
 
 public class Charakter : BaseObject
 {
-    private string name;
+    private string   abteilung;
+    private FileData bild;
+    private string   name;
+
+    private int sortIndex;
 
     public Charakter(Session session)
             : base(session) { }
@@ -15,4 +19,25 @@ public class Charakter : BaseObject
         get => name;
         set => SetPropertyValue(nameof(Name), ref name, value);
     }
+
+    public FileData Bild
+    {
+        get => bild;
+        set => SetPropertyValue(nameof(Bild), ref bild, value);
+    }
+
+    public int SortIndex
+    {
+        get => sortIndex;
+        set => SetPropertyValue(nameof(SortIndex), ref sortIndex, value);
+    }
+
+    public string Abteilung
+    {
+        get => abteilung;
+        set => SetPropertyValue(nameof(Abteilung), ref abteilung, value);
+    }
+
+    [NonPersistent]
+    public string ImgUrl => $"data:image/{Path.GetExtension(Bild.FileName).Substring(1)};base64,{Convert.ToBase64String(Bild.Content)}";
 }
--- Frontend/Domain/DatabaseUpdate/Updater.cs
+++ Frontend/Domain/DatabaseUpdate/Updater.cs
@@ -1,5 +1,8 @@
-﻿using DevExpress.ExpressApp;
+﻿using System.Reflection;
+using CoOrga.Survivors.Domain.BusinessObjects;
+using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Updating;
+using DevExpress.Persistent.BaseImpl;
 
 namespace CoOrga.Survivors.Domain.DatabaseUpdate;
 
@@ -11,9 +14,46 @@ public class Updater : ModuleUpdater
 
     public override void UpdateDatabaseAfterUpdateSchema()
     {
+        SeedCharaktere();
+        ObjectSpace.CommitChanges();
         base.UpdateDatabaseAfterUpdateSchema();
     }
-    
+
+    private void SeedCharaktere()
+    {
+        SeedCharakter("Development", "Alex", 1);
+        SeedCharakter("Development", "Tobias", 2);
+        SeedCharakter("Development", "Christian", 3);
+        SeedCharakter("Cheffs", "Björn", 1);
+        SeedCharakter("Kabelfuzzis", "Jens", 1);
+    }
+
+    private void SeedCharakter(string abteilung, string name, int sortIndex)
+    {
+        var charakter = ObjectSpace.GetObjectsQuery<Charakter>()
+                                   .FirstOrDefault(c => c.Name == name)
+                        ?? ObjectSpace.CreateObject<Charakter>();
+
+        if (!ObjectSpace.IsNewObject(charakter))
+            return;
+
+        charakter.Name      = name;
+        charakter.Abteilung = abteilung;
+        charakter.SortIndex = sortIndex;
+        charakter.Bild      = new FileData(charakter.Session);
+        LoadCharakterBildInto(charakter.Bild, name.ToLower());
+    }
+
+    private void LoadCharakterBildInto(FileData target, string bildName)
+    {
+        var resourceName = Assembly.GetExecutingAssembly()
+                                   .GetManifestResourceNames()
+                                   .First(n => n.Contains(bildName));
+        var       imgName  = $"{bildName}{Path.GetExtension(resourceName)}";
+        using var schdream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName);
+        target.LoadFromStream(imgName, schdream);
+    }
+
     //DomainObject1 theObject = ObjectSpace.FirstOrDefault<DomainObject1>(u => u.Name == name);
     //if(theObject == null) {
     //    theObject = ObjectSpace.CreateObject<DomainObject1>();
